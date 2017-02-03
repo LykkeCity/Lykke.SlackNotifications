a7fbf992732bf011eefa4af9be3fbd15c740057c
amigin <amigin@gmail.com>
2017-02-03 20:46:29 +0200

    Made abstract layer of Slack Notifications

--- src/Lykke.SlackNotifications/SlackNotificationsSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Lykke.SlackNotifications
-{
-    public class SlackNotificationsSettings
-    {
-        public string ConnectionString { get; set; }
-        public string QueueName { get; set; }
-    }
-}
