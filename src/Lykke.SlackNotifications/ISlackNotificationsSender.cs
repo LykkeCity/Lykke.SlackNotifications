5a3ec7bc9299d1811eb11940b4fbf6c7cf30ab2e
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-09-19 17:50:07 +0300

    Added ability to send sender via extension methods

--- src/Lykke.SlackNotifications/ISlackNotificationsSender.cs
+++ src/Lykke.SlackNotifications/ISlackNotificationsSender.cs
@@ -6,23 +6,4 @@ namespace Lykke.SlackNotifications
     {
         Task SendAsync(string type, string sender, string message);
     }
-
-    public static class SlackNotificationsSenderExt
-    {
-        public static Task SendInfoAsync(this ISlackNotificationsSender src, string message)
-        {
-            return src.SendAsync("Info", ":exclamation:", message);
-        }
-
-        public static Task SendErrorAsync(this ISlackNotificationsSender src, string message)
-        {
-            return src.SendAsync("Errors", ":exclamation:", message);
-        }
-
-        public static Task SendWarningAsync(this ISlackNotificationsSender src, string message)
-        {
-            return src.SendAsync("Warning", ":warning:", message);
-        }
-    }
-
 }
---
+++ src/Lykke.SlackNotifications/SlackNotificationsSenderExt.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.SlackNotifications
+{
+    public static class SlackNotificationsSenderExt
+    {
+        public static Task SendInfoAsync(this ISlackNotificationsSender src, string message, string sender = null)
+        {
+            return src.SendAsync("Info", sender ?? ":exclamation:", message);
+        }
+
+        public static Task SendErrorAsync(this ISlackNotificationsSender src, string message, string sender = null)
+        {
+            return src.SendAsync("Errors", sender ?? ":exclamation:", message);
+        }
+
+        public static Task SendWarningAsync(this ISlackNotificationsSender src, string message, string sender = null)
+        {
+            return src.SendAsync("Warning", sender ?? ":warning:", message);
+        }
+    }
+
+}
