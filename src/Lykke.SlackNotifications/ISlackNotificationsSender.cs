de2759f79f3a1889c12817c7c55b74bdfa19c59c
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-05-30 16:10:36 +0300

    Overload of SendAsync with moment parameter has been added

--- src/Lykke.SlackNotifications/ISlackNotificationsSender.cs
+++ src/Lykke.SlackNotifications/ISlackNotificationsSender.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Lykke.SlackNotifications
 {
     public interface ISlackNotificationsSender
     {
         Task SendAsync(string type, string sender, string message);
+        Task SendAsync(DateTime moment, string type, string sender, string message);
     }
 }
